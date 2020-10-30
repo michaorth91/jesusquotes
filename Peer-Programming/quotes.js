ab04f15940ece9b246a23f5b5b6e2ea2189e29d7
michaorth91 <60327482+michaorth91@users.noreply.github.com>
2020-10-30 08:57:13 +0100

    Add files via upload

---
+++ Peer-Programming/quotes.js
@@ -0,0 +1,12 @@
+
+const quotes = [
+
+    "Man does not live on bread alone",
+    "Love your neighbour as yourself",
+    "for God loved the world so much that he gave his one and only Son, so that everyone who believes in him will not perish but have eternal life. God sent his Son into the world not to judge the world, but to save the world through him.",
+   "Love your enemies! Pray for those who persecute you! In that way, you will be acting as true children of your Father in heaven. For he gives his sunlight to both the evil and the good, and he sends rain on the just and the unjust alike",
+    "And so I tell you, keep on asking, and you will receive what you ask for. Keep on seeking, and you will find. Keep on knocking, and the door will be opened to you. For everyone who asks, receives. Everyone who seeks, finds. And to everyone who knocks, the door will be opened.",
+    "With man this is impossible, but with God all things are possible.",
+    
+
+];
